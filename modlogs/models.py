bc69e109293d82b572f3de8c7aec1d12dfe2f6e4
Ummmbacon <>
2023-01-24 13:52:55 -0600

    Add managment commands, updated models

--- modlogs/models.py
+++ modlogs/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class ModLog(models.Model):
-    unique_id = models.CharField(max_length=120, default="None")
+    unique_id = models.CharField(max_length=120)
     sub_name = models.CharField(max_length=25)
-    sub_user = models.CharField(max_length=25, default="Not Specified")
+    sub_user = models.CharField(max_length=25)
     mod_name = models.CharField(max_length=25)
     mod_action = models.CharField(max_length=20)
     mod_link = models.URLField()
-    mod_time = models.TextField()
-    mod_item = models.TextField(null=True)
+    mod_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    mod_item = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.unique_id
--- modlogs/views.py
+++ modlogs/views.py
@@ -1,6 +1,6 @@
 # modlogs/views.py
 from django.template import loader
-from django.http import HttpResponse
+from django.http import StreamingHttpResponse
 from django.views.generic import TemplateView
 
 from .models import ModLog
@@ -15,4 +15,4 @@ def index(request, subreddit):
     all_logs = ModLog.objects.filter(sub_name=subreddit)
     context = {'log_list': all_logs}
 
-    return HttpResponse(template.render(context, request))
+    return StreamingHttpResponse(template.render(context, request))
