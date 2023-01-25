650e37cf9e0ac231c6e722406b4dcda76fb3d5cd
Ummmbacon <>
2023-01-24 20:04:26 -0600

    updated template, static files, order in query

--- modlogs/views.py
+++ modlogs/views.py
@@ -2,6 +2,7 @@
 from django.template import loader
 from django.http import StreamingHttpResponse
 from django.views.generic import TemplateView
+from datetime import datetime
 
 from .models import ModLog
 
@@ -12,7 +13,7 @@ class Home(TemplateView):
 
 def index(request, subreddit):
     template = loader.get_template('index.html')
-    all_logs = ModLog.objects.filter(sub_name=subreddit)
+    all_logs = ModLog.objects.filter(sub_name=subreddit).order_by('-mod_time__date')
     context = {'log_list': all_logs}
 
     return StreamingHttpResponse(template.render(context, request))
